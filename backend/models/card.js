1725967a188ba86197faa1a54071fd97e7bfc48e
AW <rooms3d@aol.com>
2023-08-19 15:54:16 +0300

    6666

--- backend/models/card.js
+++ backend/models/card.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -10,6 +11,10 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: [true, 'Поле обязательно к заполнению'],
+    validate: {
+      validator: (v) => validator.isUrl(v),
+      message: 'Некорректный URL',
+    },
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
--- backend/routes/card.js
+++ backend/routes/card.js
@@ -9,7 +9,7 @@ router.get('/cards', getCards);
 router.post('/cards', celebrate({
   body: Joi.object().keys({
     name: Joi.string().required().min(2).max(30),
-    link: Joi.string().required().uri(),
+    link: Joi.string().required(),
   }),
 }), postCard);
 router.delete('/cards/:cardId', celebrate({
