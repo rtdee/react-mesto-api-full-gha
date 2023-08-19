eb00b5fe2fd7d7e6dc3c4b6d5c1b22d3c95056fe
AW <rooms3d@aol.com>
2023-08-19 14:49:36 +0300

    turn off validator for a test backend

--- backend/models/card.js
+++ backend/models/card.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
+// const validator = require('validator');
 
 const cardSchema = new mongoose.Schema({
   name: {
@@ -11,10 +11,10 @@ const cardSchema = new mongoose.Schema({
   link: {
     type: String,
     required: [true, 'Поле обязательно к заполнению'],
-    validate: {
+    /* validate: {
       validator: (v) => validator.isUrl(v),
       message: 'Некорректный URL',
-    },
+    }, */
   },
   owner: {
     type: mongoose.Schema.Types.ObjectId,
