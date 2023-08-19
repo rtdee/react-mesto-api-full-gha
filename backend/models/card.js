e9eb573b037c9940f2aa949ac4ead5d416c845ac
AW <rooms3d@aol.com>
2023-08-19 16:19:11 +0300

    array

--- backend/models/card.js
+++ backend/models/card.js
@@ -16,7 +16,7 @@ const cardSchema = new mongoose.Schema({
     required: true,
   },
   likes: {
-    type: [mongoose.Schema.Types.ObjectId],
+    type: Array,
     ref: 'user',
     default: [],
   },
