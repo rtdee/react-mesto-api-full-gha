952f79cfafcd0bc24c07f429382566ce02b0d772
AW <rooms3d@aol.com>
2023-08-19 11:46:30 +0300

    sregwsergwsergwsrg

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,8 +11,7 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    // res.header('Access-Control-Allow-Origin', origin);
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', origin);
   }
 
   const { method } = req;
--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -99,7 +99,7 @@ class Api {
 export const api = new Api({
   baseUrl: 'https://rtdback.nomoreparties.co',
   headers: {
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': 'origin',
     'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE, OPTIONS',
     'Access-Control-Allow-Headers': 'Origin, Content-Type, X-Auth-Token,  Accept, Authorization, X-Requested-With',
     'Accept': 'application/json',
