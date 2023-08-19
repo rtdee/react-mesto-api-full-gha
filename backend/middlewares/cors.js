25701837aa21d8934472afccf84f133b0be6dd5a
AW <rooms3d@aol.com>
2023-08-19 11:45:17 +0300

    asfeeafef

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -11,7 +11,7 @@ const allowedCors = [
 module.exports = (req, res, next) => {
   const { origin } = req.headers;
   if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+    // res.header('Access-Control-Allow-Origin', origin);
     res.header('Access-Control-Allow-Origin', '*');
   }
 
