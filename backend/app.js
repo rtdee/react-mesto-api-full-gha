c15558e72c55329a24972a80afd485bf3f33647d
AW <rooms3d@aol.com>
2023-08-20 16:30:01 +0300

    app js 4

--- backend/app.js
+++ backend/app.js
@@ -5,24 +5,24 @@ const bodyParser = require('body-parser');
 const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
-// const cors = require('cors');
+const cors = require('cors');
 const { createUser } = require('./controllers/user');
 const { login } = require('./controllers/login');
 const NotFoundError = require('./errors/not-found');
 const auth = require('./middlewares/auth');
-const corsmw = require('./middlewares/cors');
+// const corsmw = require('./middlewares/cors');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 5000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const app = express();
 
-// app.use(cors());
+app.use(cors());
 app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(corsmw());
+// app.use(corsmw());
 
 mongoose.connect(DB_URL);
 
